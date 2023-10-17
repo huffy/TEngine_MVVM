95c5c67c92e4869f0d63a29d2cde5aff80339371
hunter <hunter@drpanda.com>
2023-10-17 17:25:00 +0800

    generate view ,model, viewmodel

--- UnityProject/Assets/TEngine/Editor/UI/GenerateScriptLogic.cs
+++ UnityProject/Assets/TEngine/Editor/UI/GenerateScriptLogic.cs
@@ -33,10 +33,29 @@ public class GenerateScriptLogic
     {
         return "OnSlider" + varName.Replace("m_slider", string.Empty) + "Change";
     }
+    
+    public static string GetInputFieldFuncName(string varName)
+    {
+        return "OnInputField" + varName.Replace("m_inputField", string.Empty) + "Change";
+    }
+    
+    public static string GetDropdownFuncName(string varName)
+    {
+        return "OnDropdown" + varName.Replace("m_dropdown", string.Empty) + "Change";
+    }
+    
+    public static string GetScrollbarFuncName(string varName)
+    {
+        return "OnScrollbar" + varName.Replace("m_scrollbar", string.Empty) + "Change";
+    }
+    
+    public static string GetScrollRectFuncName(string varName)
+    {
+        return "OnScrollRect" + varName.Replace("m_scrollRect", string.Empty) + "Change";
+    }
 
     public static void WriteScript(Transform root, Transform child, ref StringBuilder strVar,
-        ref StringBuilder strBind, ref StringBuilder strOnCreate,
-        ref StringBuilder strCallback, bool isUniTask)
+        ref StringBuilder strBind, ref StringBuilder strOnCreate)
     {
         string varName = child.name;
         string componentName = string.Empty;
@@ -56,20 +75,23 @@ public class GenerateScriptLogic
         string varPath = GetRelativePath(child, root);
         if (!string.IsNullOrEmpty(varName))
         {
-            strVar.Append("\t\tprivate " + componentName + " " + varName + ";\n");
             switch (componentName)
             {
                 case "Transform":
+                    strVar.Append("\t\tprivate " + componentName + " " + varName + ";\n");
                     strBind.Append($"\t\t\t{varName} = FindChild(\"{varPath}\");\n");
                     break;
                 case "GameObject":
-                    strBind.Append($"\t\t\t{varName} = FindChild(\"{varPath}\").gameObject;\n");
+                    strVar.Append("\t\tprivate " + componentName + " " + varName + "Go;\n");
+                    strBind.Append($"\t\t\t{varName}Go = FindChild(\"{varPath}\").gameObject;\n");
                     break;
                 case "AnimationCurve":
+                    strVar.Append("\t\tprivate " + componentName + " " + varName + ";\n");
                     strBind.Append(
                         $"\t\t\t{varName} = FindChildComponent<AnimCurveObject>(\"{varPath}\").m_animCurve;\n");
                     break;
                 default:
+                    strVar.Append("\t\tprivate " + componentName + " " + varName + ";\n");
                     strBind.Append($"\t\t\t{varName} = FindChildComponent<{componentName}>(\"{varPath}\");\n");
                     break;
             }
@@ -77,36 +99,170 @@ public class GenerateScriptLogic
             if (componentName == "Button")
             {
                 string varFuncName = GetBtnFuncName(varName);
-                if (isUniTask)
-                {
-                    strCallback.Append("\n");
-                    strOnCreate.Append($"\t\t\t{varName}.onClick.AddListener(UniTask.UnityAction({varFuncName}));\n");
-                    strCallback.Append($"\t\tprivate async UniTaskVoid {varFuncName}()\n");
-                    strCallback.Append("\t\t{\n await UniTask.Yield();\n\t\t}\n");
-                }
-                else
-                {
-                    strCallback.Append("\n");
-                    strOnCreate.Append($"\t\t\t{varName}.onClick.AddListener({varFuncName});\n");
-                    strCallback.Append($"\t\tprivate void {varFuncName}()\n");
-                    strCallback.Append("\t\t{\n\t\t}\n");
-                }
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onClick).To(vm => vm." + varFuncName +
+                                   ");\n");
             }
             else if (componentName == "Toggle")
             {
                 string varFuncName = GetToggleFuncName(varName);
-                strCallback.Append("\n");
-                strOnCreate.Append($"\t\t\t{varName}.onValueChanged.AddListener({varFuncName});\n");
-                strCallback.Append($"\t\tprivate void {varFuncName}(bool isOn)\n");
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onValueChanged).To<bool>(vm => vm." +
+                                   varFuncName + ");\n");
+            }
+            else if (componentName == "Slider")
+            {
+                string varFuncName = GetSliderFuncName(varName);
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onValueChanged).To<float>(vm => vm." +
+                                   varFuncName + ");\n");
+            }
+            else if (componentName == "InputField")
+            {
+                string varFuncName = GetInputFieldFuncName(varName);
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onValueChanged).To<string>(vm => vm." +
+                                   varFuncName + ");\n");
+            }
+            else if (componentName == "Text")
+            {
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.text).To(vm => vm." +
+                                   varName.Replace(rule.uiElementRegex, "") + ");\n");
+            }
+            else if (componentName == "Image")
+            {
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.sprite).To(vm => vm." +
+                                   varName.Replace(rule.uiElementRegex, "") + ").WithSprite();\n");
+            }
+            else if (componentName == "RawImage")
+            {
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.texture).To(vm => vm." +
+                                   varName.Replace(rule.uiElementRegex, "") + ").WithTexture();\n");
+            }
+            else if (componentName == "Dropdown")
+            {
+                string varFuncName = GetDropdownFuncName(varName);
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onValueChanged).To<int>(vm => vm." +
+                                   varFuncName + ");\n");
+            }
+            else if (componentName == "Scrollbar")
+            {
+                string varFuncName = GetScrollbarFuncName(varName);
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onValueChanged).To<float>(vm => vm." +
+                                   varFuncName + ");\n");
+            }
+            else if (componentName == "ScrollRect")
+            {
+                string varFuncName = GetScrollRectFuncName(varName);
+                strOnCreate.Append("\n");
+                strOnCreate.Append("\t\t\tbindingSet.Bind(" + varName + ").For(v => v.onValueChanged).To<Vector2>(vm => vm." +
+                                   varFuncName + ");\n");
+            }
+        }
+    }
+    
+    public static void WriteScriptViewModel(Transform child, ref StringBuilder strVar, ref StringBuilder strOnCreate,
+        ref StringBuilder strCallback)
+    {
+        string varName = child.name;
+        string componentName = string.Empty;
+
+        var rule = SettingsUtils.GetScriptGenerateRule().Find(t => varName.StartsWith(t.uiElementRegex));
+
+        if (rule != null)
+        {
+            componentName = rule.componentName;
+        }
+
+        if (componentName == string.Empty)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(varName))
+        {
+            if (componentName == "Button")
+            {
+                string varFuncName = GetBtnFuncName(varName);
+                strCallback.Append($"\t\tpublic void {varFuncName}()\n");
+                strCallback.Append("\t\t{\n\t\t}\n");
+            }
+            else if (componentName == "Toggle")
+            {
+                string varFuncName = GetToggleFuncName(varName);
+                strCallback.Append($"\t\tpublic void {varFuncName}(bool isToggle)\n");
                 strCallback.Append("\t\t{\n\t\t}\n");
             }
             else if (componentName == "Slider")
             {
                 string varFuncName = GetSliderFuncName(varName);
-                strCallback.Append("\n");
-                strOnCreate.Append($"\t\t\t{varName}.onValueChanged.AddListener({varFuncName});\n");
-                strCallback.Append($"\t\tprivate void {varFuncName}(float value)\n");
+                strCallback.Append($"\t\tpublic void {varFuncName}(float value)\n");
                 strCallback.Append("\t\t{\n\t\t}\n");
+            }
+            else if (componentName == "InputField")
+            {
+                string varFuncName = GetInputFieldFuncName(varName);
+                strCallback.Append($"\t\tpublic void {varFuncName}(string text)\n");
+                strCallback.Append("\t\t{\n\t\t}\n");
+            }
+            else if (componentName == "Text" || componentName == "Image" || componentName == "RawImage")
+            {
+                strOnCreate.Append("\n");
+                strVar.Append("\t\tpublic string " +  varName.Replace(rule.uiElementRegex, "") + "\n");
+                strVar.Append("\t\t{\n");
+                strVar.Append("\t\t\tget { return _model." + varName.Replace(rule.uiElementRegex, "") + "; }\n");
+                strVar.Append("\t\t\tset { this.Set(ref _model." + varName.Replace(rule.uiElementRegex, "") + ", value); }\n");
+                strVar.Append("\t\t}\n");
+                
+            }
+            else if (componentName == "Dropdown")
+            {
+                string varFuncName = GetDropdownFuncName(varName);
+                strCallback.Append($"\t\tpublic void {varFuncName}(int value)\n");
+                strCallback.Append("\t\t{\n\t\t}\n");
+            }
+            else if (componentName == "Scrollbar")
+            {
+                string varFuncName = GetScrollbarFuncName(varName);
+                strCallback.Append($"\t\tpublic void {varFuncName}(float progress)\n");
+                strCallback.Append("\t\t{\n\t\t}\n");
+            }
+            else if (componentName == "ScrollRect")
+            {
+                string varFuncName = GetScrollRectFuncName(varName);
+                strCallback.Append($"\t\tpublic void {varFuncName}(Vecter2 valueDelta)\n");
+                strCallback.Append("\t\t{\n\t\t}\n");
+            }
+        }
+    }
+        
+    public static void WriteScriptModel(Transform child, ref StringBuilder strVar)
+    {
+        string varName = child.name;
+        string componentName = string.Empty;
+
+        var rule = SettingsUtils.GetScriptGenerateRule().Find(t => varName.StartsWith(t.uiElementRegex));
+
+        if (rule != null)
+        {
+            componentName = rule.componentName;
+        }
+
+        if (componentName == string.Empty)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(varName))
+        {
+            if (componentName == "Text" || componentName == "Image" || componentName == "RawImage")
+            {
+                strVar.Append("\t\tpublic string " + varName.Replace(rule.uiElementRegex, "") + ";\n");
             }
         }
     }
--- UnityProject/Assets/TEngine/Editor/UI/PrefabErgodic.cs
+++ UnityProject/Assets/TEngine/Editor/UI/PrefabErgodic.cs
@@ -13,8 +13,7 @@ public class PrefabErgodic
             StringBuilder strVar = new StringBuilder();
             StringBuilder strBind = new StringBuilder();
             StringBuilder strOnCreate = new StringBuilder();
-            StringBuilder strCallback = new StringBuilder();
-            Ergodic(root, root, ref strVar, ref strBind, ref strOnCreate, ref strCallback, isUniTask);
+            Ergodic(root, root, ref strVar, ref strBind, ref strOnCreate, isUniTask);
             StringBuilder strFile = new StringBuilder();
 
             if (includeListener)
@@ -26,7 +25,10 @@ public class PrefabErgodic
                 {
                     strFile.Append("using Cysharp.Threading.Tasks;\n");
                 }
-
+                strFile.Append("using Framework.Binding;\n");
+                strFile.Append("using Framework.Binding.Builder;\n");
+                strFile.Append("using Framework.Binding.Contexts;\n");
+                strFile.Append("using Framework.Contexts;\n");
                 strFile.Append("using UnityEngine;\n");
                 strFile.Append("using UnityEngine.UI;\n");
                 strFile.Append("using TEngine;\n\n");
@@ -39,25 +41,30 @@ public class PrefabErgodic
 
             // 脚本工具生成的代码
             strFile.Append("\t\t#region 脚本工具生成的代码\n");
+            strFile.Append($"\t\tprivate {root.name}ViewModel _viewModel;\n");
             strFile.Append(strVar);
             strFile.Append("\t\tpublic override void ScriptGenerator()\n");
             strFile.Append("\t\t{\n");
+            strFile.Append("\t\t\tbase.ScriptGenerator();\n");
+            strFile.Append($"\t\t\t_viewModel = new {root.name}ViewModel();\n");
+            strFile.Append("\t\t\tIBindingContext bindingContext = transform.BindingContext();\n");
+            strFile.Append("\t\t\tbindingContext.DataContext = _viewModel;\n");
+            strFile.Append("\t\t\tBindingSet<" + root.name + ", " + root.name + "ViewModel> bindingSet = this.CreateBindingSet<" + root.name + ", " + root.name + "ViewModel>();\n");
             strFile.Append(strBind);
             strFile.Append(strOnCreate);
+            strFile.Append("\t\t\tbindingSet.Build();\n");
             strFile.Append("\t\t}\n");
             strFile.Append("\t\t#endregion");
 
             if (includeListener)
             {
                 strFile.Append("\n\n");
-                // #region 事件
-                strFile.Append("\t\t#region 事件\n");
-                strFile.Append(strCallback);
-                strFile.Append("\t\t#endregion\n\n");
 
                 strFile.Append("\t}\n");
                 strFile.Append("}\n");
                 SaveScript(root.name, strFile.ToString());
+                GenerateViewModel(root);
+                GenerateModel(root);
             }
             else
             {
@@ -76,10 +83,9 @@ public class PrefabErgodic
             StringBuilder strVar = new StringBuilder();
             StringBuilder strBind = new StringBuilder();
             StringBuilder strOnCreate = new StringBuilder();
-            StringBuilder strCallback = new StringBuilder();
-            Ergodic(root, root, ref strVar, ref strBind, ref strOnCreate, ref strCallback, isUniTask);
+            Ergodic(root, root, ref strVar, ref strBind, ref strOnCreate, isUniTask);
             StringBuilder strFile = new StringBuilder();
-            string widgetName = root.name.Replace("m_item", "") + "ItemWidget";
+            string widgetName = root.name.Replace("m_item", "");
 #if ENABLE_TEXTMESHPRO
                     strFile.Append("using TMPro;\n");
 #endif
@@ -91,6 +97,7 @@ public class PrefabErgodic
             strFile.Append("using UnityEngine;\n");
             strFile.Append("using UnityEngine.UI;\n");
             strFile.Append("using TEngine;\n\n");
+            strFile.Append("using Framework.Binding;\n\n");
             strFile.Append($"namespace {SettingsUtils.GetUINameSpace()}\n");
             strFile.Append("{\n");
             strFile.Append("\tpublic class " + widgetName + " : UIWidget\n");
@@ -98,23 +105,25 @@ public class PrefabErgodic
 
             // 脚本工具生成的代码
             strFile.Append("\t\t#region 脚本工具生成的代码\n");
+            strFile.Append("\t\tpublic " + widgetName + "ViewModel ViewModel\n");
+            strFile.Append("\t\t{\n");
+            strFile.Append("\t\t\tget => (" + widgetName + "ViewModel)transform.GetDataContext(); \n");
+            strFile.Append("\t\t\tset => transform.SetDataContext(value); \n");
+            strFile.Append("\t\t}\n");
             strFile.Append(strVar);
             strFile.Append("\t\tpublic override void ScriptGenerator()\n");
             strFile.Append("\t\t{\n");
+            strFile.Append("\t\t\tbase.ScriptGenerator();\n");
+            strFile.Append("\t\t\tvar bindingSet = this.CreateBindingSet<" + widgetName + ", " + widgetName + "ViewModel>();\n");
             strFile.Append(strBind);
             strFile.Append(strOnCreate);
+            strFile.Append("\t\t\tbindingSet.Build();\n");
             strFile.Append("\t\t}\n");
             strFile.Append("\t\t#endregion");
 
             strFile.Append("\n\n");
-            // #region 事件
-            strFile.Append("\t\t#region 事件\n");
-            strFile.Append(strCallback);
-            strFile.Append("\t\t#endregion\n\n");
-
-            strFile.Append("\t}\n");
             strFile.Append("}\n");
-         
+            strFile.Append("\t}\n");
             SaveScript(widgetName, strFile.ToString());
             return widgetName;
         }
@@ -122,28 +131,133 @@ public class PrefabErgodic
         return "";
     }
 
-    private static void Ergodic(Transform root, Transform transform, ref StringBuilder strVar,
-        ref StringBuilder strBind, ref StringBuilder strOnCreate,
-        ref StringBuilder strCallback, bool isUniTask)
+     public static string GenerateViewModel(Transform root)
+     {
+         if (root != null)
+         {
+             StringBuilder strVar = new StringBuilder();
+             StringBuilder strOnCreate = new StringBuilder();
+             StringBuilder strCallback = new StringBuilder();
+             ErgodicViewModel(root, ref strVar,  ref strOnCreate, ref strCallback);
+             StringBuilder strFile = new StringBuilder();
+             string widgetName = root.name.Replace("m_item", "");
+             strFile.Append("using Framework.ViewModels;\n");
+             strFile.Append("using UnityEngine;\n");
+             strFile.Append($"namespace {SettingsUtils.GetUINameSpace()}\n");
+             strFile.Append("{\n");
+             strFile.Append("\tpublic class " + widgetName + "ViewModel : ViewModelBase\n");
+             strFile.Append("\t{\n");
+
+             // 脚本工具生成的代码
+             strFile.Append("\t\t#region 脚本工具生成的代码\n");
+             strFile.Append($"\t\tprivate {widgetName}Model _model;\n");
+             strFile.Append(strVar);
+             strFile.Append($"\t\tpublic {widgetName}ViewModel ()\n");
+             strFile.Append("\t\t{\n"); 
+             strFile.Append($"\t\t\t_model = new {widgetName}Model();\n");
+             strFile.Append("\t\t}\n");
+             strFile.Append("\t\t#endregion");
+
+             strFile.Append("\n\n");
+             // #region 事件
+             strFile.Append("\t\t#region 事件\n");
+             strFile.Append(strCallback);
+             strFile.Append("\t\t#endregion\n\n");
+             strFile.Append("\t}\n");
+             strFile.Append("}\n");
+             SaveScript(widgetName + "ViewModel", strFile.ToString());
+
+             return widgetName + "ViewModel";
+         }
+
+         return "";
+     }
+     
+     public static void GenerateModel(Transform root)
+     {
+         if (root != null)
+         {
+             StringBuilder strVar = new StringBuilder();
+             ErgodicModel(root, ref strVar);
+             StringBuilder strFile = new StringBuilder();
+             string widgetName = root.name.Replace("m_item", "");
+             strFile.Append("using UnityEngine;\n");
+             strFile.Append($"namespace {SettingsUtils.GetUINameSpace()}\n");
+             strFile.Append("{\n");
+             strFile.Append("\tpublic class " + widgetName + "Model\n");
+             strFile.Append("\t{\n");
+
+             // 脚本工具生成的代码
+             strFile.Append("\t\t#region 脚本工具生成的代码\n");
+             strFile.Append(strVar);
+
+             strFile.Append("\t\t#endregion");
+
+             strFile.Append("\n\n");
+
+             strFile.Append("\t}\n");
+             strFile.Append("}\n");
+             SaveScript(widgetName + "Model", strFile.ToString());
+         }
+     }
+
+     private static void Ergodic(Transform root, Transform transform, ref StringBuilder strVar,
+        ref StringBuilder strBind, ref StringBuilder strOnCreate, bool isUniTask)
     {
         for (int i = 0; i < transform.childCount; ++i)
         {
             Transform child = transform.GetChild(i);
-            GenerateScriptLogic.WriteScript(root, child, ref strVar, ref strBind, ref strOnCreate, ref strCallback,
-                isUniTask);
+            GenerateScriptLogic.WriteScript(root, child, ref strVar, ref strBind, ref strOnCreate);
             if (child.name.StartsWith("m_item"))
             {
                 // 子 Item 不再往下遍历
                 string componentName = GenerateWidget(child, isUniTask);
                 string varName = "m_item" + componentName;
                 strVar.Append("\t\tprivate " + componentName + " " + varName + ";\n");
-                strBind.Append($"\t\t\t{varName} = CreateWidget<{componentName}>(\"{child.name}\");\n");
+                strBind.Append($"\t\t\t{varName} = CreateWidget<{componentName}>({child.name}Go);\n");
+                strOnCreate.Append($"\t\t\tbindingSet.Bind({varName}).For(v => v.ViewModel).To(vm => vm.M_{componentName}ViewModel);\n");
+            
                 continue;
             }
 
-            Ergodic(root, child, ref strVar, ref strBind, ref strOnCreate, ref strCallback, isUniTask);
+            Ergodic(root, child, ref strVar, ref strBind, ref strOnCreate, isUniTask);
         }
     }
+     
+     private static void ErgodicViewModel(Transform transform, ref StringBuilder strVar, ref StringBuilder strOnCreate,
+         ref StringBuilder strCallback)
+     {
+         for (int i = 0; i < transform.childCount; ++i)
+         {
+             Transform child = transform.GetChild(i);
+             GenerateScriptLogic.WriteScriptViewModel(child, ref strVar, ref strOnCreate, ref strCallback);
+             if (child.name.StartsWith("m_item"))
+             {
+                 string componentName = GenerateViewModel(child);
+                 string varName = "M_" + componentName;
+                 GenerateModel(child);
+                 strVar.Append("\t\tpublic " + componentName + " " + varName + ";\n");
+                 continue;
+             }
+
+             ErgodicViewModel(child, ref strVar, ref strOnCreate, ref strCallback);
+         }
+     }
+     
+     private static void ErgodicModel(Transform transform, ref StringBuilder strVar)
+     {
+         for (int i = 0; i < transform.childCount; ++i)
+         {
+             Transform child = transform.GetChild(i);
+             GenerateScriptLogic.WriteScriptModel(child, ref strVar);
+             if (child.name.StartsWith("m_item"))
+             {
+                 continue;
+             }
+
+             ErgodicModel(child, ref strVar);
+         }
+     }
 
     private static void SaveScript(string prefabName, string content)
     {
